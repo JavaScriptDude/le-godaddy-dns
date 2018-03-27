042b41199ed0d954bf809b643d0c2b7386a80e8f
JavaScriptDude <tim.c.quinn@gmail.com>
2018-03-27 02:06:06 -0400

    Update godaddy.py

--- godaddy.py
+++ godaddy.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+# fork of https://github.com/josteink/le-godaddy-dns
 
 import os
 import sys
@@ -6,6 +7,8 @@ import logging
 from tld import get_tld
 import time
 import godaddypy
+from OpenSSL import crypto
+import configparser
 
 if "GD_KEY" not in os.environ:
     raise Exception("Missing Godaddy API-key in GD_KEY environment variable! Please register one at https://developer.godaddy.com/keys/")
@@ -13,15 +16,15 @@ if "GD_KEY" not in os.environ:
 if "GD_SECRET" not in os.environ:
     raise Exception("Missing Godaddy API-secret in GD_SECRET environment variable! Please register one at https://developer.godaddy.com/keys/")
 
-api_key = os.environ["GD_KEY"]
-api_secret = os.environ["GD_SECRET"]
-my_acct = godaddypy.Account(api_key=api_key, api_secret=api_secret)
+my_acct = godaddypy.Account(
+    api_key=os.environ["GD_KEY"], 
+    api_secret=os.environ["GD_SECRET"]
+)
 client = godaddypy.Client(my_acct)
 
 logger = logging.getLogger(__name__)
 logger.addHandler(logging.StreamHandler())
 logger.setLevel(logging.INFO)
-
 
 def _get_zone(domain):
     d = get_tld(domain,as_object=True,fix_protocol=True)
@@ -32,6 +35,33 @@ def _get_subdomain_for(domain, zone):
     subdomain = domain[0:(-len(zone)-1)]
     return subdomain
 
+def _add_dns_rec(domain, token):
+    challengedomain = "_acme-challenge." + domain
+    logger.info(" + Adding TXT record for {0} to '{1}'.".format(challengedomain, token))
+    zone = _get_zone(challengedomain)
+    # logger.info("Zone to update: {0}".format(zone))
+    subdomain = _get_subdomain_for(challengedomain, zone)
+    # logger.info("Subdomain name: {0}".format(subdomain))
+
+    record = {
+        'name': subdomain,
+        'data': token,
+        'ttl': 600,
+        'type': 'TXT'
+    }
+    result=None
+    try:
+        result = client.add_record(zone, record)
+    except godaddypy.client.BadResponse as err:
+        msg=str(err)
+        if msg.find('DUPLICATE_RECORD') > -1:
+            logger.info(" + . Duplicate record found. Skipping.")
+            return
+        logger.warn("Error returned {0}.".format(err))
+    if result is not True:
+        logger.warn("Error updating record for domain {0}.".format(domain))
+    else:
+        logger.info(" + . Record added")
 
 def _update_dns(domain, token):
     challengedomain = "_acme-challenge." + domain
@@ -53,10 +83,22 @@ def _update_dns(domain, token):
 
 
 def create_txt_record(args):
+
+    dehyd_config=loadDehydratedConfig()    
+    gdpy_use_patch_api = True if dehyd_config is not None \
+        and "gdpy_use_patch_api" in dehyd_config \
+        and dehyd_config['gdpy_use_patch_api'] == 'yes' else False
+    logger.info("gdpy_use_patch_api: {0}".format(gdpy_use_patch_api))
+
     for i in range(0, len(args), 3):
         domain, token = args[i], args[i+2]
-        _update_dns(domain, token)
+        if gdpy_use_patch_api:
+            _add_dns_rec(domain, token)
+        else:
+            _update_dns(domain, token)
+        time.sleep(2)
     # a sleep is needed to allow DNS propagation
+    logger.info(" + Sleeping to wait for DNS propagation")
     time.sleep(30)
 
 
@@ -69,14 +111,86 @@ def delete_txt_record(args):
         if domain == "":
             logger.warn("Error deleting record, the domain argument is empty")
         else:
-            _update_dns(domain, "null")
+            _update_dns(domain, "_dehydrated-was-here_")
 
 
 def deploy_cert(args):
     domain, privkey_pem, cert_pem, fullchain_pem, chain_pem, timestamp = args
     logger.info(' + ssl_certificate: {0}'.format(fullchain_pem))
     logger.info(' + ssl_certificate_key: {0}'.format(privkey_pem))
+
+    dehyd_config=loadDehydratedConfig()    
+    gdpy_gen_pfx = True if dehyd_config is not None \
+        and "gdpy_gen_pfx" in dehyd_config \
+        and dehyd_config['gdpy_gen_pfx'] == 'yes' else False
+    logger.info("gdpy_gen_pfx: {0}".format(gdpy_gen_pfx))
+
+    if gdpy_gen_pfx is True:
+        if "PKCS_PASSWORD" not in os.environ:
+            raise Exception("Missing PKCS Password in PKCS_PASSWORD environment variable!")
+        # Create PFX for IIS
+        pfx_pem = generate_pfx(args)
+    
     return
+
+def generate_pfx(args):
+    domain, privkey_pem, cert_pem, fullchain_pem, chain_pem, timestamp = args
+
+    logger.info("args: {0}".format(args))
+    pfx_pem=cert_pem.replace("cert.pem", "cert.pfx")
+
+    # Read in cert
+    cert = crypto.load_certificate(
+        crypto.FILETYPE_PEM, open(cert_pem, 'rt').read()
+    )
+
+    # Read in private key
+    privkey = crypto.load_privatekey(
+        crypto.FILETYPE_PEM, open(privkey_pem, 'rt').read()
+    )
+
+    # Generate PFX
+    pfx = crypto.PKCS12Type()
+    pfx.set_privatekey(privkey)
+    pfx.set_certificate(cert)
+    pfxdata = pfx.export(os.environ["PKCS_PASSWORD"])
+
+    # Write PFX to file
+    with open(pfx_pem, 'wb') as f:
+        f.write(pfxdata)
+    
+    logger.info(' + PFX File Created: {0}'.format(pfx_pem))
+    return pfx_pem
+
+def loadDehydratedConfig():
+    # Find dehydrated config file
+    # . First check env variable
+    logger.info("\n\n!!! os.environ['DEHYDRATED_CONFIG]: {0}\n\n".format(os.environ['DEHYDRATED_CONFIG']))
+
+
+    dehyConfigPath=None
+    if "DEHYD_CONFIG" in os.environ:
+        dehyConfigPath=os.environ["DEHYD_CONFIG"]
+
+    else:
+        # . Next check cwd for file named config
+        cwd=os.getcwd()
+        dehyConfigPath=cwd + '/config'
+
+    logger.info("dehyConfigPath: {0}".format(dehyConfigPath))
+
+    if os.path.isfile(dehyConfigPath) is False:
+        logger.info(" + godaddy.py: dehydrated config not found or set correctly in DEHYD_CONFIG. Ignoring.")
+        return None
+    else:
+        config_string=None
+        with open(dehyConfigPath, 'r') as f:
+            config_string = '[dummy]\n' + f.read()
+        cp = configparser.ConfigParser()
+        cp.read_string(config_string)
+        dehyd_config=cp._sections['dummy']
+        # logger.info("dehyd_config: {0}".format(dehyd_config))
+    return dehyd_config
 
 
 def invalid_challenge(args):
@@ -92,6 +206,7 @@ def request_failure(args):
 
 
 def main(argv):
+    
     ops = {
         'deploy_challenge': create_txt_record,
         'clean_challenge' : delete_txt_record,
